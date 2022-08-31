ec1c701c8d220de5872d44873353aa0a6843e263
SergiiPachkovskyi <SergiiPachkovskyi@gmail.com>
2022-08-31 22:50:31 +0300

    edit test_auth.py

--- proj_news/tests/test_auth.py
+++ proj_news/tests/test_auth.py
@@ -1,5 +1,3 @@
-import json
-
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
 
@@ -13,7 +11,7 @@ class AuthTestCase(APITestCase):
             "password": "user123456"
         }
         client = APIClient()
-        client.post("/api/v1/token-auth/users/", data=json.dumps(data), content_type="application/json")
+        client.post("/api/v1/token-auth/users/", data)
 
     def test_signup(self):
         data = {
@@ -21,7 +19,7 @@ class AuthTestCase(APITestCase):
             "username": "test_user",
             "password": "user123456"
         }
-        response = self.client.post("/api/v1/token-auth/users/", data=json.dumps(data), content_type="application/json")
+        response = self.client.post("/api/v1/token-auth/users/", data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_jwt(self):
@@ -29,12 +27,12 @@ class AuthTestCase(APITestCase):
             "username": "init_user",
             "password": "user123456"
         }
-        response = self.client.post("/api/v1/jwt-auth/", data=json.dumps(data), content_type="application/json")
+        response = self.client.post("/api/v1/jwt-auth/", data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         data = {
             "refresh": response.data['refresh']
         }
-        response = self.client.post("/api/v1/jwt-auth/refresh/", data=json.dumps(data), content_type="application/json")
+        response = self.client.post("/api/v1/jwt-auth/refresh/", data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
