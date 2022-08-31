ec442ec39130fd90938f324782ae10bdf7b2d209
SergiiPachkovskyi <SergiiPachkovskyi@gmail.com>
2022-08-31 22:44:20 +0300

    add test_auth.py

---
+++ proj_news/tests/test_auth.py
@@ -0,0 +1,40 @@
+import json
+
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
+
+
+class AuthTestCase(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        data = {
+            "email": "init_user@gmail.com",
+            "username": "init_user",
+            "password": "user123456"
+        }
+        client = APIClient()
+        client.post("/api/v1/token-auth/users/", data=json.dumps(data), content_type="application/json")
+
+    def test_signup(self):
+        data = {
+            "email": "user@gmail.com",
+            "username": "test_user",
+            "password": "user123456"
+        }
+        response = self.client.post("/api/v1/token-auth/users/", data=json.dumps(data), content_type="application/json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_jwt(self):
+        data = {
+            "username": "init_user",
+            "password": "user123456"
+        }
+        response = self.client.post("/api/v1/jwt-auth/", data=json.dumps(data), content_type="application/json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        data = {
+            "refresh": response.data['refresh']
+        }
+        response = self.client.post("/api/v1/jwt-auth/refresh/", data=json.dumps(data), content_type="application/json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
